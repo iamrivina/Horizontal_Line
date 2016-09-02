d53a4502d8681f25326b64c7e04136935247930c
iamrivina <rivinakumar@gmail.com>
2016-09-02 16:18:13 +0530

    Initial commit

---
+++ src/com/thoughtworks/Main.java
@@ -0,0 +1,15 @@
+package com.thoughtworks;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt();
+        for(int i=0; i<n; i++)
+        {
+            System.out.print("*");
+        }
+    }
+}
